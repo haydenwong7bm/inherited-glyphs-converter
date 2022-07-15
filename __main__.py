80dd01a4ab094f4bdb9578b0fd73191a86d746fb
Hayden Wong <51369959+haydenwong7bm@users.noreply.github.com>
2022-07-15 21:03:15 +0800

    Fix some bugs

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ def _file_to_dict(file):
     conversion_dict = {}
     
     for line in file:
-        key_value = line.rsplit('\n').split('\t')
+        key_value = line.rstrip('\n').split('\t')
         key = key_value[0]
         value = key_value[1]
         
@@ -21,9 +21,9 @@ def _file_to_dict(file):
         
     return conversion_dict
 
-CONVERSION_DICT_SCS = _file_to_dict(open('conversion-tables/variants-scs.txt', 'rt', encoding='utf-8'))
-CONVERSION_DICT_J = _file_to_dict(open('conversion-tables/variants-compatibility-j.txt', 'rt', encoding='utf-8'))
-CONVERSION_DICT_K = _file_to_dict(open('conversion-tables/variants-compatibility-k.txt', 'rt', encoding='utf-8'))
+CONVERSION_DICT_SCS = _file_to_dict(open('conversion-tables/inherited_variants.txt', 'rt', encoding='utf-8'))
+CONVERSION_DICT_J = _file_to_dict(open('conversion-tables/j-compatibility_variants.txt', 'rt', encoding='utf-8'))
+CONVERSION_DICT_K = _file_to_dict(open('conversion-tables/k-compatibility_variants.txt', 'rt', encoding='utf-8'))
 
 def convert(string: str, use_j=False, use_k=False) -> str:
     for key, value in CONVERSION_DICT_SCS.items():
@@ -41,5 +41,5 @@ def convert(string: str, use_j=False, use_k=False) -> str:
 
 if __name__ == '__main__':
     filename, file_ext = os.path.splitext(args.file)
-    with open(args.file, 'rt') as file_read, open(f'{filename}-converted.{file_ext}', 'rt') as file_write:
+    with open(args.file, 'rt') as file_read, open(f'{filename}-converted{file_ext}', 'wt') as file_write:
         file_write.write(convert(file_read.read(), args.use_j, args.use_k))
