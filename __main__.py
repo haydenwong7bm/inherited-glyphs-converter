539b58ca700d9299b9eebf3e0058a60cb6468376
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2023-02-08 23:50:25 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -10,7 +10,7 @@ parser.add_argument('-o', '--options', action='store_true')
 parser.add_argument('-c', '--comp', default=False)
 parser.add_argument('-s', '--supp', default=False)
 parser.add_argument('-i', '--inherited', action='store_true')
-parser.add_argument('-I', '--ivs', default=False)
+parser.add_argument('-I', '--ivs', nargs='*')
 
 args = parser.parse_args()
 
@@ -20,16 +20,9 @@ if not args.options: # default options
     args.inherited = True
     args.ivs = False
 
-ivs = []
-for char in args.ivs:
-    if char == 'a':
-        ivs.append('aj1')
-    elif char == 'm':
-        ivs.append('mj')
-    
 filename, file_ext = path.splitext(path.basename(args.file))
 with (open(args.file, 'rt') as input_file,
       open(f'{filename}-converted{file_ext}', 'wt') as output_file):
     contents = input_file.read()
-    converted = convert(contents, use_supp_planes=args.supp, use_compatibility=compatibility_args, convert_inherited=args.inherited, use_ivs=ivs)
+    converted = convert(contents, use_supp_planes=args.supp, use_compatibility=args.comp, convert_inherited=args.inherited, use_ivs=args.ivs)
     output_file.write(converted)
