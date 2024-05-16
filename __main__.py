ee34f54ada0690678aa0df814142e25fb51f1134
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2024-05-16 11:34:48 +0800

    Update __main__.py
    Now force UTF-8 encoding

--- __main__.py
+++ __main__.py
@@ -26,8 +26,8 @@ if not args.file:
 
 for file in args.file:
     filename, file_ext = path.splitext(file)
-    with (open(file, 'rt') as input_file,
-        open(f'{filename}-converted{file_ext}', 'wt') as output_file):
+    with (open(file, 'rt', encoding='utf-8') as input_file,
+        open(f'{filename}-converted{file_ext}', 'wt', encoding='utf-8') as output_file):
         contents = input_file.read()
         converted = convert(contents, supp_planes=args.supp, compatibility=args.compat, convert_not_unifiable=args.not_unifiable, alternate=args.alternate, academic_correct=args.academic_correct, ivs=args.ivs, punctation_align_center=args.punctation)
         output_file.write(converted)
